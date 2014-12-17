38b65120e3e76ee7077bb6e47beaf8859e3286b9
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-12-18 00:43:02 +0100

    Starting GAE project

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ gpluscommutetracker
 Google+ Location Sharing tracker, specialized in commute detecting, tracking and predicting
 
 ##Not functional yet!
+
+###Will be hosted on gpluscommutetracker.appspot.com
--- gpluscommutetracker.py
+++ gpluscommutetracker.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ArchieT'
+import cgi
 from google.appengine.api import users
 import webapp2
+
+MAIN_PAGE_HTML = file('main-html.html').read()
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		user = users.get_current_user()
 		if user:
 			self.response.headers['Content-Type'] = 'text/html'
-			self.response.write('Zażółć gęślą jaźń, ' + user.nickname())
+			self.response.write(MAIN_PAGE_HTML + user.nickname())
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
+class Adding(webapp2.RequestHandler):
+	def adding(self):
+		self.response.write('<html><body>You entered:<pre>')
+		self.response.write(
+			cgi.escape(self.request.get('profileID'))+\
+			" "+cgi.escape(self.request.get('forever'))+\
+			" until "+\
+			cgi.escape(self.request.get('until'))+\
+			" every "+cgi.escape(self.request.get('every'))+"minutes"
+		)
+		self.response.write('</pre></body></html>')
 
 application = webapp2.WSGIApplication([
 	('/', MainPage),
+	('/adding', Adding)
 ], debug=True)
