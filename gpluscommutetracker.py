80ebbd5f824418b92ea2ac881f75ba8a2003fa6a
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-12-18 00:13:41 +0100

    Starting GAE project: auth

--- gpluscommutetracker.py
+++ gpluscommutetracker.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
-__author__ = 'mf'
+__author__ = 'ArchieT'
+from google.appengine.api import users
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/html'
-		self.response.write('Zażółć gęślą jaźń')
+		user = users.get_current_user()
+		if user:
+			self.response.headers['Content-Type'] = 'text/html'
+			self.response.write('Zażółć gęślą jaźń, ' + user.nickname())
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp2.WSGIApplication([
 	('/', MainPage),
