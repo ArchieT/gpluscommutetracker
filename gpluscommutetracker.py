935ff006a73b6892a79e5362b5e1ed69a79b1b5b
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-12-19 05:16:49 +0100

    Starting GAE project

--- gpluscommutetracker.py
+++ gpluscommutetracker.py
@@ -5,15 +5,17 @@ import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
 import webapp2
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+	extensions=['jinja2.ext.autoescape'],
+	autoescape=True
+)
 
 #MAIN_PAGE_HTML = file('main-html.html').read()
 MAIN_PAGE_FOOTER = """\
-	<form action="/adding?%s" method="post">
-		<div>Profile ID: <input name="profileid" type="text"></div>
-		<div> track <input type="checkbox" name="forever" value="forever"> forever / until <input name="until" type="datetime"></div>
-		<div> every <input type="number" name="every"> minutes</div>
-		<div><input type="submit" value="Start tracking"></div>
-	</form>
+
 </body>
 </html>
 """
@@ -22,25 +24,32 @@ class MainPage(webapp2.RequestHandler):
 	def get(self):
 		user = users.get_current_user()
 		if user:
-			self.response.headers['Content-Type'] = 'text/html'
+			#self.response.headers['Content-Type'] = 'text/html'
 			#self.response.write(MAIN_PAGE_HTML + user.nickname())
-			self.response.write('<html><body bgcolor="#FFFFCC">')
+			#self.response.write('<html><body bgcolor="#FFFFCC">')
 			databaza_name = self.request.get('databaza_name', 'dbinstancje_ongoing')
 			instances_query = Instance.query(ancestor=ndb.Key('databaza','dbinstancje_ongoing')).order(-Instance.creationdate)
 			instances = instances_query.fetch(1000)
 
-			for instance in instances:
-				self.response.write("owner: %s profileID: %s until: %s every: %s forever: %s creationdate: %s" % (
-					instance.owner.nickname(),
-					instance.profileID(),
-					instance.until(),
-					instance.every(),
-					instance.forever(),
-					instance.creationdate()
-				))
+			#for instance in instances:
+			#	self.response.write("owner: %s profileID: %s until: %s every: %s forever: %s creationdate: %s" % (
+			#		instance.owner.nickname(),
+			#		instance.profileID(),
+			#		instance.until(),
+			#		instance.every(),
+			#		instance.forever(),
+			#		instance.creationdate()
+			#	))
 
-			adding_query_params = urllib.urlencode({'databaza_name': databaza_name})
-			self.response.write(MAIN_PAGE_FOOTER % adding_query_params)
+			template_values = {
+				'instances': instances,
+				'databaza_name': urllib.quote_plus(databaza_name),
+			}
+
+			#adding_query_params = urllib.urlencode({'databaza_name': databaza_name})
+			#self.response.write(MAIN_PAGE_FOOTER % adding_query_params)
+			template = JINJA_ENVIRONMENT.get_template('index.html')
+			self.response.write(template.render(template_values))
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
 class Adding(webapp2.RequestHandler):
